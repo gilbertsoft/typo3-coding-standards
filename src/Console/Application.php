d217766bd0f14b4799ae9e1d148a40a28053dfed
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2022-08-15 10:33:18 +0200

    Revert "[TASK] Release workflow and scripts (#53)"
    This reverts commit 84f39507e307f34267bd1e538427b8790c67a8aa.

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -35,7 +35,7 @@ final class Application extends BaseApplication
     /**
      * @var string
      */
-    public const VERSION = '0.6.0-dev';
+    public const VERSION = '0.6.0-DEV';
 
     /**
      * getcwd() equivalent which always returns a string
